2abff016f2b80bb41458ea5569234fd01a2137a5
John Cheadle <jcheadle.rti@gmail.com>
2022-07-06 16:20:49 -0400

    Updating output style of files

--- call_mds.py
+++ call_mds.py
@@ -7,11 +7,15 @@ def main():
     output_path = "outputs"
     output_file = "mds_results.json"
     mds_results = query_mds()
-    #clean_mds_results = transform_mds_dict(mds_results)
+    mds_studies_list,mds_covid_list = transform_mds_dict(mds_results)
 
-    # Export to JSON
-    with open(f"{output_path}/{output_file}", "w") as stream:
-        json.dump(mds_results,indent=2,fp=stream)
+    # Export Files to JSON
+    with open(f"{output_path}/studies_{output_file}", "w") as stream:
+        json.dump(mds_studies_list,indent=2,fp=stream)
+
+    with open(f"{output_path}/covid_{output_file}", "w") as stream:
+        json.dump(mds_covid_list,indent=2,fp=stream)
+
 
 def query_mds():
 
@@ -42,8 +46,45 @@ def query_mds():
     return(results_obj)
 
 def transform_mds_dict(mds_result):
-    # Do some transformations
-    return(mds_results)
+    # Initialize lists
+    mds_studies_list = []
+    mds_covid_list = []
+    
+
+    # Iterate over each item
+    for k,v in mds_result.items():
+
+        # Initialize covid and dict
+        new_dict = {}
+        covid = False
+
+        # Get main metadata
+        metadata = v['gen3_discovery']
+
+        # COVID Check
+        for elem in metadata['tags']:
+            if 'COVID 19' in elem.values():
+                covid = True # set covid true
+
+        # Strip Consent
+        full_accession = metadata['dbgap_accession']
+        accession = full_accession[:full_accession.find('.c')]
+
+        # Write Values
+        new_dict["Acession"] = accession
+        new_dict["Cohort Abbreviation"] = metadata['short_name']
+        new_dict["Name"] = metadata['full_name']
+        new_dict["Description"] = metadata['study_description']
+        new_dict["Consent Code"] = metadata['dbgap_consent']
+        new_dict["Consent Short"] = metadata['dbgap_consent_text']
+        new_dict["Subject Count"] = metadata['_subjects_count']
+
+        if covid:
+            mds_covid_list.append(new_dict)
+        else:
+            mds_studies_list.append(new_dict)
+
+    return(mds_studies_list,mds_covid_list)
 
 if __name__ == "__main__":
     main()
